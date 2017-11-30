897a6de206ff77d4c2baca475d078dda0e5f8360
MV Admin <mvadmin@MVCC-GB-MAC-15.local>
2017-11-29 19:44:49 -0800

    almost finished

---
+++ static/js/code.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+  var randomNames = [];
+  var allNames = [];
+
+  $("#enterButton").click(function() {
+    randomNames = [];
+    allNames = [
+      $("#text1").val(),
+      $("#text2").val(),
+      $("#text3").val(),
+      $("#text4").val(),
+      $("#text5").val(),
+      $("#text6").val(),
+      $("#text7").val(),
+      $("#text8").val(),
+      $("#text9").val()
+    ];
+
+    for (var i = 0; i < 9; i++) {
+      var arrayNumber = randomArrayNumber();
+
+      randomNames.push(allNames[arrayNumber]);
+
+      allNames.splice(arrayNumber, 1);
+    }
+
+    console.log(randomNames);
+
+    for (var j = 0; j < randomNames.length; j++) {
+      if (j != 8) {
+        console.log(randomNames[j] + "-" + randomNames[j + 1]);
+      } else {
+        console.log(randomNames[8] + "-" + randomNames[0]);
+      }
+    }
+
+    sendMail();
+  });
+
+  function randomArrayNumber() {
+    return Math.floor(Math.random() * allNames.length);
+  }
+
+  function sendMail() {
+
+    console.log("hi")
+    var link = "mailto:ivan@burgert.com"
+      + "?cc=blah@blah.com"
+      + "&subject=" + escape("Your secret santa")
+      + "&body=" + escape(randomNames[1])
+    ;
+    window.location.href = link;
+
+  }
+
+
+});
