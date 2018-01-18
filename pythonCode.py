75741135704769a36a97e355a378085c31e2d8df
MV Admin <mvadmin@MVCC-GB-MAC-15.local>
2018-01-17 19:22:06 -0800

    added favicon

--- pythonCode.py
+++ pythonCode.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
   return render_template("index.html")
-  # return "hello"
-  
+
+@app.route("/favicon.ico")
+def favicon():
+	return send_from_directory(os.path.join(app.root_path, "static", "images"), "favicon.ico", mimetype="image/png")
   
   
   
