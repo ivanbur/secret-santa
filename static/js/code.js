5e26d86be6e6445fd6f009566f9026a3c5b6fbc8
MV Admin <mvadmin@MVCC-GB-MAC-15.local>
2018-01-17 18:45:20 -0800

    added support for more people v1.1

--- static/js/code.js
+++ static/js/code.js
@@ -1,10 +1,65 @@
+var randomNames = [];
+var allNames = [];
+var amountOfPeople = 0;
+
+function enterButton() {
+  console.log("hey");
+
+  randomNames = [];
+  allNames = [];
+
+  for (var names = 1; names <= amountOfPeople; names++) {
+    allNames.push($("#text" + names).val());
+    console.warn("whatup")
+  }
+
+  for (var i = 0; i < amountOfPeople; i++) {
+    var arrayNumber = Math.floor(Math.random() * allNames.length);
+
+    console.error("1 - allNames: " + allNames.length + " - randomNames: " + randomNames.length)
+
+    randomNames.push(allNames[arrayNumber]);
+
+    console.error("2 - allNames: " + allNames.length + " - randomNames: " + randomNames.length)
+
+    allNames.splice(arrayNumber, 1);
+
+    console.error("3 - allNames: " + allNames.length + " - randomNames: " + randomNames.length)
+  }
+
+  console.log(randomNames);
+
+  for (var j = 0; j < randomNames.length; j++) {
+    if (j != (randomNames.length - 1) ) {
+      console.log(randomNames[j] + "-" + randomNames[j + 1]);
+    } else {
+      console.log(randomNames[j] + "-" + randomNames[0]);
+    }
+  }
+
+  sendMail();
+};
+
+function sendMail() {
+  for (var k = 0; k < randomNames.length; k++) {
+    if (k != (randomNames.length - 1) ) {
+        emailjs.send("default_service","secret_santa",{
+        email: randomNames[k], 
+        bodyText: randomNames[k + 1]
+      });
+    } else {
+      emailjs.send("default_service","secret_santa",{
+        email: randomNames[k], 
+        bodyText: randomNames[0]
+      });
+    }
+  }
+
+  console.log("Sent!");
+    
+}
+
 $(document).ready(function() {
-
-  var randomNames = [];
-  var allNames = [];
-  var amountOfPeople = "";
-  
-
 
   $("#numberOfPeopleButton").click(function() {
     amountOfPeople = prompt("How many people are participating?");
@@ -14,61 +69,6 @@ $(document).ready(function() {
     }
 
     $("button").remove();
-    $("body").append("<br/><button id = 'enterButton' type = 'button'>Enter</button>")
+    $("body").append("<br/><button id = 'enterButton' type = 'button' onclick='enterButton()'>Enter</button>")
   });
-
-
-  $("#enterButton").click(function() {
-    randomNames = [];
-    allNames = [];
-
-    for (var names = 1; names <= amountOfPeople; names++) {
-      allNames.push($("#text" + names).val());
-    }
-
-    for (var i = 0; i < 9; i++) {
-      var arrayNumber = randomArrayNumber();
-
-      randomNames.push(allNames[arrayNumber]);
-
-      allNames.splice(arrayNumber, 1);
-    }
-
-    console.log(randomNames);
-
-    for (var j = 0; j < randomNames.length; j++) {
-      if (j != (randomNames.length - 1) ) {
-        console.log(randomNames[j] + "-" + randomNames[j + 1]);
-      } else {
-        console.log(randomNames[j] + "-" + randomNames[0]);
-      }
-    }
-
-    sendMail();
-  });
-
-  function randomArrayNumber() {
-    return Math.floor(Math.random() * allNames.length);
-    console.log(allNames.length);
-  }
-
-  function sendMail() {
-    for (var k = 0; k < randomNames.length; k++) {
-      if (k != (randomNames.length - 1) ) {
-          emailjs.send("default_service","secret_santa",{
-          email: randomNames[k], 
-          bodyText: randomNames[k + 1]
-        });
-      } else {
-        emailjs.send("default_service","secret_santa",{
-          email: randomNames[k], 
-          bodyText: randomNames[0]
-        });
-      }
-    }
-
-    console.log("Sent!");
-    
-  }
-
 });
